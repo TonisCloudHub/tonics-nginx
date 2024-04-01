72f3b5c611dab75b6c2bf8ca58025fa4200d0bad
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2024-04-01 01:03:29 +0100

    Update script-main.sh
    Replaced lxc with incus

--- .github/bookwarm/scripts/script-main.sh
+++ .github/bookwarm/scripts/script-main.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+# Init incus
+sudo incus admin init --auto
+
 # Launch Instance
-lxc launch images:debian/bookworm/amd64 tonics-nginx
+sudo incus launch images:debian/bookworm/amd64 tonics-nginx
 
 # Nginx Dependencies
-lxc exec tonics-nginx -- apt install -y curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+sudo incus exec tonics-nginx -- apt install -y curl gnupg2 ca-certificates lsb-release debian-archive-keyring
 
 # Sign Repo
-lxc exec tonics-nginx -- bash -c "curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null"
-lxc exec tonics-nginx -- bash -c "gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg"
-lxc exec tonics-nginx -- bash -c "echo 'deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian bookworm nginx' | tee /etc/apt/sources.list.d/nginx.list | tee /etc/apt/sources.list.d/nginx.list"
-lxc exec tonics-nginx -- bash -c "echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n' | tee /etc/apt/preferences.d/99nginx"
+sudo incus exec tonics-nginx -- bash -c "curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null"
+sudo incus exec tonics-nginx -- bash -c "gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg"
+sudo incus exec tonics-nginx -- bash -c "echo 'deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian bookworm nginx' | tee /etc/apt/sources.list.d/nginx.list | tee /etc/apt/sources.list.d/nginx.list"
+sudo incus exec tonics-nginx -- bash -c "echo -e 'Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n' | tee /etc/apt/preferences.d/99nginx"
 
 # Install Nginx
-lxc exec tonics-nginx -- bash -c "apt-get update -y && apt install -y nginx"
+sudo incus exec tonics-nginx -- bash -c "apt-get update -y && apt install -y nginx"
 
 # Start Nginx
-lxc exec tonics-nginx -- bash -c "sudo nginx"
+sudo incus exec tonics-nginx -- bash -c "sudo nginx"
 
 # Clean Debian Cache
-lxc exec tonics-nginx -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
+sudo incus exec tonics-nginx -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # Nginx Version
-NginxVersion=$(lxc exec tonics-nginx -- nginx -v |& sed 's/nginx version: nginx\///')
+NginxVersion=$(sudo incus exec tonics-nginx -- nginx -v |& sed 's/nginx version: nginx\///')
 
 # Publish Image
-mkdir images && lxc stop tonics-nginx && lxc publish tonics-nginx --alias tonics-nginx
+mkdir images && sudo incus stop tonics-nginx && sudo incus publish tonics-nginx --alias tonics-nginx
 
 # Export Image
-lxc start tonics-nginx
-lxc image export tonics-nginx images/nginx-bookworm-$NginxVersion
+sudo incus start tonics-nginx
+sudo incus image export tonics-nginx images/nginx-bookworm-$NginxVersion
 
 # Image Info
-lxc image info tonics-nginx >> images/info.txt && ls -la images
+sudo incus image info tonics-nginx >> images/info.txt && ls -la images
